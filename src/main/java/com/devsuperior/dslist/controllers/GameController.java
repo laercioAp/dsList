f339d5eb5c5db0d5b8533fb09b9efababeaf006f
Laercio Aparecido Pedroso <laerciogu00@gmail.com>
2023-05-09 21:30:40 -0300

    fix: endpoint get by id

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,5 +1,6 @@
 package com.devsuperior.dslist.controllers;
 
+import com.devsuperior.dslist.dto.GameDTO;
 import com.devsuperior.dslist.dto.GameMinDTO;
 import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
@@ -18,7 +19,13 @@ public class GameController {
     @Autowired
     private GameService gameService;
 
-    @GetMapping
+    @GetMapping(value = "/{id}")
+    public GameDTO findById(@PathVariable Long id) {
+        GameDTO result = gameService.findById(id);
+        return result;
+    }
+
+    @GetMapping()
     public List<GameMinDTO> findAll() {
         List<GameMinDTO> result = gameService.finAll();
         return result;
